eb873c7f7a8f6240b60651bbbb770e6d3173eeea
Ivar Refsdal <ivar.refsdal@sikt.no>
2026-01-08 13:24:17 +0100

    Try speed up fzf

--- src/com/github/ivarref/finddep.clj
+++ src/com/github/ivarref/finddep.clj
@@ -287,7 +287,7 @@
                  (.printStackTrace t)
                  (throw t)))]
     (if-let [v (fz/fzf {:preview-fn (fn [selected]
-                                      (with-out-str (find (assoc opts :name (str selected)))))}
+                                      (with-out-str (find (assoc opts :libs libs :name (str selected)))))}
                        (into [] (mapv str (reverse (sort (keys libs))))))]
       (find (assoc opts :name v))
       (println "Nothing selected, exiting"))))
